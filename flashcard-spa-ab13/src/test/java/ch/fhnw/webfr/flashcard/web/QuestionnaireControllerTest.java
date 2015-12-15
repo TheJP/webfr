71a52771f570b8e1b19b3250624f89f80b4b0212
TheJP <jp_55555@hotmail.com>
2015-12-16 00:13:47 +0100

    Wrote some unit tests

--- flashcard-spa-ab13/src/test/java/ch/fhnw/webfr/flashcard/web/QuestionnaireControllerTest.java
+++ flashcard-spa-ab13/src/test/java/ch/fhnw/webfr/flashcard/web/QuestionnaireControllerTest.java
@@ -1,10 +1,12 @@
 package ch.fhnw.webfr.flashcard.web;
 
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Arrays;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import ch.fhnw.webfr.flashcard.domain.Questionnaire;
 import ch.fhnw.webfr.flashcard.persistence.QuestionnaireRepository;
@@ -67,6 +70,81 @@ public class QuestionnaireControllerTest {
                 .header("Accept", "application/json")
         )
         		.andExpect(status().isNotFound());
-    }	
+    }
 
+	@Test
+	public void createQuestionnaire() throws Exception {
+		Questionnaire q = new QuestionnaireBuilder(2L)
+			.title("title")
+			.description("aaa")
+			.build();
+		when(questionnaireRepositoryMock.save(any(Questionnaire.class))).thenReturn(q);
+		mockMvc.perform(
+			post("/questionnaires/")
+				.header("Accept", "application/json")
+				.contentType("application/json")
+				.content("{ \"title\": \"title\", \"description\": \"aaa\" }"))
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("$.id", is(2)))
+			.andExpect(jsonPath("$.title", is("title")))
+			.andExpect(jsonPath("$.description", is("aaa")));
+		verify(questionnaireRepositoryMock, times(1)).save(
+			eq(new QuestionnaireBuilder(0L).title("title").description("aaa").build()));
+	}
+
+	@Test
+	public void createQuestionnaireError() throws Exception {
+		when(questionnaireRepositoryMock.save(any(Questionnaire.class))).thenReturn(null);
+		mockMvc.perform(
+			post("/questionnaires/")
+				.header("Accept", "application/json")
+				.contentType("application/json")
+				.content("{ \"title\": \"title\", \"description\": \"aa\" }"))
+			.andExpect(status().isInternalServerError());
+	}
+
+	@Test
+	public void readAllQuestionnaires() throws Exception {
+		Questionnaire q1 = new QuestionnaireBuilder(1L)
+			.title("title")
+			.description("aaa")
+			.build();
+		Questionnaire q2 = new QuestionnaireBuilder(2L)
+				.title("title2")
+				.description("aaa2")
+				.build();
+		when(questionnaireRepositoryMock.findAll()).thenReturn(Arrays.asList(q1, q2));
+		mockMvc.perform(
+			get("/questionnaires/").header("Accept", "application/json"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$[0].id", is(1)))
+			.andExpect(jsonPath("$[0].title", is("title")))
+			.andExpect(jsonPath("$[0].description", is("aaa")))
+			.andExpect(jsonPath("$[1].id", is(2)))
+			.andExpect(jsonPath("$[1].title", is("title2")))
+			.andExpect(jsonPath("$[1].description", is("aaa2")));
+		verify(questionnaireRepositoryMock, times(1)).findAll();
+	}
+
+//	@Test
+//	public void updateQuestionnaire() throws Exception {
+//		Questionnaire q1 = new QuestionnaireBuilder(1L)
+//			.title("title")
+//			.description("aaa")
+//			.build();
+//		Questionnaire q2 = new QuestionnaireBuilder(2L)
+//				.title("title2")
+//				.description("aaa2")
+//				.build();
+//		when(questionnaireRepositoryMock.findAll()).thenReturn(Arrays.asList(q1, q2));
+//		mockMvc.perform(
+//			get("/questionnaires/").header("Accept", "application/json"))
+//			.andExpect(status().isOk())
+//			.andExpect(jsonPath("$[0].id", is(1)))
+//			.andExpect(jsonPath("$[0].title", is("title")))
+//			.andExpect(jsonPath("$[0].description", is("aaa")))
+//			.andExpect(jsonPath("$[1].id", is(2)))
+//			.andExpect(jsonPath("$[1].title", is("title2")))
+//			.andExpect(jsonPath("$[1].description", is("aaa2")));
+//	}
 }
