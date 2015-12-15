62cb6d846a37bc8951362563139d3746914649c9
TheJP <jp_55555@hotmail.com>
2015-12-15 18:25:14 +0100

    Implemented read and create

--- flashcard-spa-ab13/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
+++ flashcard-spa-ab13/src/main/java/ch/fhnw/webfr/flashcard/web/QuestionnaireController.java
@@ -1,9 +1,14 @@
 package ch.fhnw.webfr.flashcard.web;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +22,18 @@ public class QuestionnaireController {
 
 	@Autowired
 	private QuestionnaireRepository repository;
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Questionnaire> create(@Valid @RequestBody Questionnaire questionnaire){
+		Questionnaire q = repository.save(questionnaire);
+		if(q == null){ return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); }
+		return new ResponseEntity<>(q, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Questionnaire>> getAll(){
+		return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
+	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Questionnaire> get(@PathVariable("id") Long id){
